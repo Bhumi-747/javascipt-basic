03453f338792eac63bc62f8dc19de39642737d49
Bhumika Sagore <bhumikasagore747@gmail.com>
2025-02-20 03:43:35 +0000

    dataType variable

---
+++ .devcontainer/01_basic/02_variable.js
@@ -0,0 +1,16 @@
+//"use strict"
+const accountId =12345
+let accountEmail ="bhumika@google.com"
+var accountPassword = "3434"
+accountCity ="anjad"
+let accountState;
+//accountId = 4
+
+
+
+accountEmail ="hello.com"
+accountPassword = "1212"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
---
+++ .devcontainer/01_basic/03_DataType.js
@@ -0,0 +1,9 @@
+let age = 18
+let name ="bhumii"
+
+
+
+console.log(4+3)
+console.log(typeof age);
+console.log(typeof null);  //boject
+console.log(typeof undefined)  // undefined
