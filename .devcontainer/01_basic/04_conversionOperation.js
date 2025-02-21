4b76814a5419529064f5eb3474b9320d961f5b7e
Bhumika Sagore <bhumikasagore747@gmail.com>
2025-02-21 03:37:59 +0000

    datatype and conversion

---
+++ .devcontainer/01_basic/04_conversionOperation.js
@@ -0,0 +1,41 @@
+/* let score = undefined
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNUmber = Number(score)
+console.log(typeof valueNUmber)
+
+console.log( valueNUmber)
+let value = true
+ console.log(typeof value)
+ let sun = 33
+ let stringNum = String(sun)
+ console.log(stringNum)
+ console.log(typeof stringNum) */
+
+ //      *****************************OPERATION************************ 
+    
+
+ let v=3
+ let nv = -v
+ console.log(nv)
+ console.log(2+2)
+ console.log(2-2)
+ console.log(2*2)
+ console.log(2**3)
+ console.log(2%3)
+ console.log(2/3)
+
+ 
+ let str1 = "hello"
+ let str2 = " bhumiii"
+ let str3 = str1 + str2
+ console.log(str3)
+console.log(true)
+let a=3
+//let b=a++
+let b=++a
+
+console.log(a,b)
+console.log(typeof null)
---
+++ .devcontainer/01_basic/datatype-summary.js
@@ -0,0 +1,11 @@
+ const value = Symbol('123')
+ const id =Symbol('123')
+
+ console.log(value == id)
+ const bigNumber = 123246685664243567677n
+ console.log(typeof bigNumber)
+
+
+ const arr =["bhumii","gouri","vishaha"];
+ console.log(arr)
+
